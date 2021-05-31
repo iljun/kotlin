9a6f7b581c8fa18eede1844e141e5541dbdda08f
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2021-05-31 19:19:54 +0300

    Forbid TYPE and TYPE_PARAMETER as possible OptIn targets

--- compiler/testData/diagnostics/testsWithStdLib/experimental/annotation.fir.kt
+++ compiler/testData/diagnostics/testsWithStdLib/experimental/annotation.fir.kt
@@ -5,13 +5,13 @@
 package api
 
 @RequiresOptIn(level = RequiresOptIn.Level.WARNING)
-@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.TYPEALIAS,
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.TYPEALIAS,
         AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.BINARY)
 annotation class ExperimentalAPI
 
 @ExperimentalAPI
-@Target(AnnotationTarget.TYPE, AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.TYPEALIAS,
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.TYPEALIAS,
         AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.BINARY)
 annotation class EAnno
