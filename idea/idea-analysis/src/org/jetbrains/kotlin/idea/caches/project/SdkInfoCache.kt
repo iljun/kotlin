e9360ad3d69923fc9fa2d2d48fec4425a62cb31d
Vladimir Dolzhenko <vladimir.dolzhenko@jetbrains.com>
2021-06-07 13:45:06 +0200

    review comments

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/project/SdkInfoCache.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/project/SdkInfoCache.kt
@@ -8,6 +8,7 @@ package org.jetbrains.kotlin.idea.caches.project
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
+import com.jetbrains.rd.util.concurrentMapOf
 import org.jetbrains.kotlin.analyzer.ModuleInfo
 import org.jetbrains.kotlin.caches.project.cacheInvalidatingOnRootModifications
 import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
@@ -36,61 +37,62 @@ interface SdkInfoCache {
 }
 
 class SdkInfoCacheImpl(private val project: Project) : SdkInfoCache {
-    private val cache: MutableMap<ModuleInfo, SdkInfo?>
+    @JvmInline
+    value class SdkDependency(val sdk: SdkInfo?)
+
+    private val cache: MutableMap<ModuleInfo, SdkDependency>
         // Note: it's ok to use thread-unsafe map, because 'doFindSdk' is pure
-        get() = project.cacheInvalidatingOnRootModifications { mutableMapOf() }
+        get() = project.cacheInvalidatingOnRootModifications { concurrentMapOf() }
 
     override fun findOrGetCachedSdk(moduleInfo: ModuleInfo): SdkInfo? {
         // get an operate on the fixed instance of a cache to avoid case of roots modification in the middle of lookup
         val instance = cache
-        // synchronized is needed as well for `doFindSdk` that does multiple writes during lookup
-        synchronized(instance) {
-            if (!instance.containsKey(moduleInfo)) {
-                instance[moduleInfo] = doFindSdk(instance, moduleInfo)
-            }
 
-            return instance[moduleInfo]
+        if (!instance.containsKey(moduleInfo)) {
+            instance[moduleInfo] = doFindSdk(instance, moduleInfo)
         }
+
+        return instance[moduleInfo]?.sdk
     }
 
-    private fun doFindSdk(cache: MutableMap<ModuleInfo, SdkInfo?>, moduleInfo: ModuleInfo): SdkInfo? {
-        moduleInfo.safeAs<SdkInfo>()?.let { return it }
+    private fun doFindSdk(cache: MutableMap<ModuleInfo, SdkDependency>, moduleInfo: ModuleInfo): SdkDependency {
+        moduleInfo.safeAs<SdkDependency>()?.let { return it }
 
         val libraryDependenciesCache = LibraryDependenciesCache.getInstance(this.project)
+        val visitedModuleInfos = mutableSetOf<ModuleInfo>()
 
-        // return value (graph, sdk) is used to mark entire graph could be resolved to sdk (when sdk is not a null)
-        fun depthLookup(graphs: ArrayDeque<List<ModuleInfo>>): Pair<List<ModuleInfo>?, SdkInfo?> {
-            // graphs is a stack of graphs is used to implement DFS without recursion
-            // it depends on a number of libs, that could be > 10k for a huge monorepos
+        // graphs is a stack of paths is used to implement DFS without recursion
+        // it depends on a number of libs, that could be > 10k for a huge monorepos
+        val graphs = ArrayDeque<List<ModuleInfo>>().also {
+            // initial graph item
+            it.add(listOf(moduleInfo))
+        }
+
+        val (path, sdkInfo) = run {
             while (graphs.isNotEmpty()) {
                 ProgressManager.checkCanceled()
                 // graph of DFS from the root i.e from `moduleInfo`
                 val graph = graphs.poll()
 
                 val last = graph.last()
-                if (cache.containsKey(last)) {
-                    // the result could be immediately returned when cache already has it
-                    cache[last]?.let { return graph to it }
-                    // or it is known that the item graph is a dead end and no reason to process it again
-                        ?: continue
-                }
+                // the result could be immediately returned when cache already has it
+                cache[last]?.let { return@run graph to it }
 
-                // don't know result yet, have to mark as a dead end to avoid repetition
-                // note: entire graph will be marked as resolved when anything deeper will be found
-                cache[last] = null
-                val dependencies = run {
+                if (!visitedModuleInfos.add(last)) continue
+
+                val dependencies = run deps@{
                     if (last is LibraryInfo) {
                         // use a special case for LibraryInfo to reuse values from a library dependencies cache
                         val (libraries, sdks) = libraryDependenciesCache.getLibrariesAndSdksUsedWith(last)
                         sdks.firstOrNull()?.let {
-                            return graph to it
+                            return@run graph to SdkDependency(it)
                         }
                         libraries
                     } else {
                         last.dependencies()
                             .also { dependencies ->
                                 dependencies.firstIsInstanceOrNull<SdkInfo>()?.let {
-                                    return graph to it
+                                    return@run graph to SdkDependency(it)
                                 }
                             }
                     }
@@ -99,24 +101,17 @@ class SdkInfoCacheImpl(private val project: Project) : SdkInfoCache {
                 dependencies.forEach { dependency ->
                     // sdk is found when some dependency is already resolved
                     cache[dependency]?.let {
-                        return (graph + dependency) to it
-                    }
-                    // otherwise add a new graph of (existed graph + dependency) as candidates for DFS lookup
-                    if (!cache.containsKey(dependency)) {
+                        return@run (graph + dependency) to it
+                    } ?: run {
+                        // otherwise add a new graph of (existed graph + dependency) as candidates for DFS lookup
                         graphs.add(graph + dependency)
                     }
                 }
             }
-            return null to null
+            return@run null to SdkDependency(null)
         }
-
-        val (stack, sdkInfo) =
-            depthLookup(ArrayDeque<List<ModuleInfo>>().also {
-                // initial graph item
-                it.add(listOf(moduleInfo))
-            })
         // when sdk is found: mark all graph elements could be resolved to the same sdk
-        sdkInfo?.let { stack?.forEach { cache[it] = sdkInfo } }
+        path?.forEach { cache[it] = sdkInfo }
         return sdkInfo
     }
 }
